0367f7064838186e0e5863794d4528c0fb28b627
sushil pandey <sushilpandeyy9@gmail.com>
2023-01-26 21:31:40 +0545

    #

--- First_program.cpp
+++ First_program.cpp
@@ -1,6 +1,6 @@
 #include<iostream> //header file for the input output funtions
 int main(){
     std::cout<<"hello world "; //cout is the object of ostream (i.e class) or output statement 
-    // cout is define in the std namespace . (it call standard library where cout is present )
+    // cout is define in the std namespace . (it call standard library where cout is present .
     return 0;
 }
